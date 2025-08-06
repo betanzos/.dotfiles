11d4729d8c7fa2b391867c33a03c584859b94770
Eduardo Betanzos <ebetanzos@hotmail.es>
2025-08-05 22:03:49 -0600

    Clone .dotfiles repo during setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+#========================================================================
+# Script name:  setup.sh
+# Description:  This script is for setting up my environment in
+#               Debian-based distros
+#
+# Author:       Eduardo Betanzos
+# Email:        ebetanzos@outlook.com
+#
+# Usage:        sh -c "$(curl -sSL https://raw.githubusercontent.com/betanzos/.dotfiles/master/setup.sh)"
+#
+#
+# Copyright © 2025  Eduardo Betanzos
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#========================================================================
+
 
 # ENSURE NEEDED PERMISSIONS
 # Prevent run as root because sometimes when executed as root the
@@ -56,7 +82,9 @@ chsh -s $(which zsh) && \
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 echo "       => Done!\n"
 
-echo "[INFO] Applying my dot files..."
+echo "[INFO] Downloading and applying my dot files..."
+cd $HOME && \
+git clone https://github.com/betanzos/.dotfiles.git && \
 cd $HOME/.dotfiles && \
 sudo stow --adopt . && \
 git restore .
