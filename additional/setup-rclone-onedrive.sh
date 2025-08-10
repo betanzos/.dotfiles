e35a945f11f52e64389cf4e57589f9070f20258f
Eduardo Betanzos <ebetanzos@hotmail.es>
2025-08-10 16:36:18 -0600

    Improve rclone setup

--- additional/setup-rclone-onedrive.sh
+++ additional/setup-rclone-onedrive.sh
@@ -3,7 +3,7 @@
 # install and configure rclone
 # we are assuming the user will configure a remote for Microsoft OneDrive named OneDrive
 sudo bash -c "$(curl -s https://rclone.org/install.sh)"
-rclone config && \
+rclone config
 
 
 # create the directory where the OneDrive remote will be mounted in
@@ -18,15 +18,15 @@ sudo chmod +x ~/.mount-onedrive.sh && \
 # create the automount configuration (on user log in)
 mkdir -p ~/.config/autostart && \
 
-echo "[DESKTOP ENTRY]\n \
-Name=onedrive-rclone\n \
-GenericName=OneDrive rclone\n \
-Comment=Auto mount OneDrive with rclone on log in\n \
-Exec=$HOME/.mount-onedrive.sh\n \
-Terminal=false\n \
-Type=Application\n \
-X-GNOME-Autostart-enabled=true" >> ~/.config/autostart/onedrive-rclone.desktop && \
+echo "[DESKTOP ENTRY]
+Name=Onedrive rclone
+Type=Application
+Comment=Auto mount OneDrive with rclone on log in
+Exec=$HOME/.mount-onedrive.sh
+X-GNOME-Autostart-enabled=true" > ~/.config/autostart/onedrive-rclone.desktop && \
+
+echo "\n[INFO] Configuration finished\n"
 
 
 # mount the OneDrive remote in the local filesystem
-source ~/.mount-onedrive.sh &
+~/.mount-onedrive.sh
