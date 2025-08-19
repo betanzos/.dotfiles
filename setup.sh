bba589225754eceb6d48faba6b88de629a55f0ba
Eduardo Betanzos <ebetanzos@hotmail.es>
2025-08-19 10:32:57 -0600

    Install net-tools

--- setup.sh
+++ setup.sh
@@ -63,6 +63,7 @@ sudo apt-get install -y \
              gimp \
              git \
              neovim \
+             net-tools \
              notepadqq \
              qbittorrent \
              stow \
