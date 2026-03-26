0a73fa9eef773ceb7722813cb5887e5ca45b63d7
Eduardo Betanzos <ebetanzos@hotmail.es>
2026-03-25 18:33:35 -0600

    Show notifications in the bottom-right corner

--- additional/setup-gnome.sh
+++ additional/setup-gnome.sh
@@ -116,6 +116,7 @@ if [[ "$XDG_CURRENT_DESKTOP" =~ "GNOME" ]]; then  # non-posix test, it need bash
     dconf write /org/gnome/shell/extensions/just-perfection/animation '0' # no animations
     dconf write /org/gnome/shell/extensions/just-perfection/clock-menu-position '1' #right
     dconf write /org/gnome/shell/extensions/just-perfection/top-panel-position '1' #bottom
+    dconf write /org/gnome/shell/extensions/just-perfection/notification-banner-position '5' #bottom end
     dconf write /org/gnome/shell/extensions/space-bar/appearance/active-workspace-background-color '"rgb(145,65,172)"' #purple
     dconf write /org/gnome/shell/extensions/space-bar/behavior/scroll-wheel '"disabled"'
     dconf write /org/gnome/shell/extensions/pip-on-top/stick 'true'
